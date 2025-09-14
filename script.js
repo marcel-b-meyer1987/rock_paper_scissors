98beb5d916a4960194d4a8cfe419d1ab49ad310d
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2025-09-14 13:21:48 +0200

    Set up files and create pseudo code for game

---
+++ script.js
@@ -0,0 +1,19 @@
+// This is a simple rock-paper-scissors game to be
+// played in the browser console
+
+// 1. getComputerChoice:
+// randomly return rock, paper or scissors
+
+// 2. getHumanChoice
+// prompt the user to enter a valid choice
+
+// 3. declare + initialize score variables
+// for humanScore and computerScore
+
+// 4. playRound
+// function to play a round: 
+// 4.1 compare getComputerChoice to getHumanChoice
+// 4.2 evaluate winner
+// 4.3 output winner + updated score board
+
+// 5. Game Loop = 5x playRound
