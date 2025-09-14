8ad7bff7f0d91b528afcb8e0383f30781a5e8e96
Marcel Meyer <23430036+marcel-b-meyer1987@users.noreply.github.com>
2025-09-14 21:02:09 +0200

    add functions for getting human + computer choices, as well as parsing random computer choices into strings

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 // This is a simple rock-paper-scissors game to be
 // played in the browser console
 
+//  util function to stringify random choices
+function stringifyChoice(choice) {
+    switch (choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors"
+            break;
+    }
+}
+
 // 1. getComputerChoice:
 // randomly return rock, paper or scissors
+function getComputerChoice() {
+    let computerChoice = Math.ceil((Math.random() * 3));
+    console.log(computerChoice);
+    return stringifyChoice(computerChoice);
+}
+
 
 // 2. getHumanChoice
 // prompt the user to enter a valid choice
+function getHumanChoice() {
+    let humanChoice = prompt("Please enter 'rock', 'paper' or 'scissors' below.","");
+    console.log(humanChoice);
+    return humanChoice;
+}
 
 // 3. declare + initialize score variables
 // for humanScore and computerScore
